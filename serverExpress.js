1b06870e98e63c9ceb43fb3c096cd432fa1a8014
ChrisIhler <Ihlerconsulting@gmail.com>
2018-11-10 19:36:42 -0800

    Refactor Express server to send different responses

--- serverExpress.js
+++ serverExpress.js
@@ -5,10 +5,21 @@ var port = process.env.PORT || 8000
 
 app.disable('x-powered-by')
 
-app.use(function(req, res) {
+app.get('/guests', function(req, res) {
   var guests = ['Mary', 'Don']
   res.send(guests)
 })
+
+app.use(function(req, res ) {
+  res.sendStatus(404)
+})
+
+
+// old combination of .get and .use
+// app.use(function(req, res) {
+//   var guests = ['Mary', 'Don']
+//   res.send(guests)
+// })
 
 
 
