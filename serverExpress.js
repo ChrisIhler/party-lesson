241ee4c3f8fb806ca7f6996a8eadba9e7c1b6429
ChrisIhler <Ihlerconsulting@gmail.com>
2018-11-10 19:56:08 -0800

    Send all guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -1,3 +1,8 @@
+//step 3 lines added
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
+
 
 var express = require('express')
 var app = express()
@@ -5,21 +10,41 @@ var port = process.env.PORT || 8000
 
 app.disable('x-powered-by')
 
+//step 3 addition. 
 app.get('/guests', function(req, res) {
-  var guests = ['Mary', 'Don']
-  res.send(guests)
-})
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
 
+    var guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  });
+});
+
+
+//step 2
+// app.get('/guests', function(req, res) {
+//   var guests = ['Mary', 'Don']
+//   res.send(guests)
+// })
+
+//step 2
 app.use(function(req, res ) {
   res.sendStatus(404)
 })
 
 
-// old combination of .get and .use
+// step 1 changed in step 2 old combination of .get and .use
 // app.use(function(req, res) {
 //   var guests = ['Mary', 'Don']
 //   res.send(guests)
 // })
+
+
+
 
 
 
